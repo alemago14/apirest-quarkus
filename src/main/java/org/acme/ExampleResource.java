3e2892e77aab0ecde8b869f3df9f928f9d60199d
Ale Maugouber <ale.maugouber@gmail.com>
2023-09-11 16:26:52 -0300

    Eventos al inicio y fin de la app

---
+++ src/main/java/org/acme/ExampleResource.java
@@ -0,0 +1,25 @@
+package org.acme;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import java.awt.*;
+
+@Path("/hello")
+public class ExampleResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from RESTEasy Reactive";
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("custom/{nombre}")
+    public String saludo(String nombre){
+        return "Hello " + nombre + " from RESTEasy Reactive";
+    }
+}
